01fa7fe5b3421d2ce37189a6d9030471aab550bd
mashkenazi <max.ashkenazi@gmail.com>
2023-07-25 19:30:43 +0300

    fixed cluster creation

--- gke_autopilot/gke_autopilot.tf
+++ gke_autopilot/gke_autopilot.tf
@@ -6,7 +6,19 @@ provider "google" {
 
 resource "google_container_cluster" "words-gke-autopilot-cluster" {
   name               = "words-gke-autopilot-cluster"
-  location           = "me-west1"
-  remove_default_node_pool = true
   initial_node_count = 1
+  remove_default_node_pool = true
+  location = "me-west1"
+}
+
+resource "google_container_node_pool" "default-pool" {
+  autoscaling {
+    max_node_count = 20
+    min_node_count = 1
+  }
+  cluster = "words-gke-autopilot-cluster"
+  initial_node_count = 1
+  name = "default-pool"
+  location = "me-west1"
+  depends_on = [google_container_cluster.words-gke-autopilot-cluster]
 }
