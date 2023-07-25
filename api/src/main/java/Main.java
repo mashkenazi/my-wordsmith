4c0104f45eb5667dff2170b40e3d68681ad80a27
mashkenazi <max.ashkenazi@gmail.com>
2023-07-26 00:04:46 +0300

    updated connection string in API

--- api/src/main/java/Main.java
+++ api/src/main/java/Main.java
@@ -9,10 +9,6 @@ import java.net.InetSocketAddress;
 import java.sql.*;
 import java.util.NoSuchElementException;
 
-public class Global {
-    public static String conn = "jdbc:postgresql://google/mydb?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=akiva-ashkenazi:me-west1:words-postgresql-instance&user=postgres&password=pass";
-}
-
 public class Main {
     public static void main(String[] args) throws Exception {
         Class.forName("org.postgresql.Driver");
@@ -23,9 +19,9 @@ public class Main {
         server.createContext("/adjective", handler(() -> randomWord("adjectives")));
         server.start();
     }
-
+    public static String conn = "jdbc:postgresql://google/mydb?socketFactory=com.google.cloud.sql.postgres.SocketFactory&cloudSqlInstance=akiva-ashkenazi:me-west1:words-postgresql-instance&user=postgres&password=pass";
     private static String randomWord(String table) {
-        try (Connection connection = DriverManager.getConnection(Global.conn)) {
+        try (Connection connection = DriverManager.getConnection(conn)) {
             try (Statement statement = connection.createStatement()) {
                 try (ResultSet set = statement.executeQuery("SELECT word FROM " + table + " ORDER BY random() LIMIT 1")) {
                     while (set.next()) {
